41b7900ca0ef537e4a480417f4a76c82c4d2f09f
Faker334 <141054619+Faker334@users.noreply.github.com>
2023-12-09 17:25:38 +0300

    Update InstController.java

--- src/main/java/com/example/inst/controllers/InstController.java
+++ src/main/java/com/example/inst/controllers/InstController.java
@@ -23,7 +23,7 @@ import java.util.List;
 public class InstController {
 
 
-    thoFactor atorization = new thoFactor("paraparapa383","Faker001");
+    thoFactor atorization = new thoFactor("paraparapa383","****");
 
 
     @GetMapping("/main")
